2b66644e2811dadc976945ae49b8b729199412aa
harish-p-exalca <harish.p@exalca.com>
2021-06-07 10:05:48 +0530

    control-center start(resolved)

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ControlCenterComponent } from './control-center/control-center.component';
 
 import { DashboardPage } from './dashboard.page';
 
@@ -7,6 +8,10 @@ const routes: Routes = [
   {
     path: '',
     component: DashboardPage
+  },
+  {
+    path:'control-center',
+    component:ControlCenterComponent
   }
 ];
 
